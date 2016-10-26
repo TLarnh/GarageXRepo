42e62131e588d7a80654a695701b84b86b59e1e6
Thomas <thomas.larnhed@gmail.com>
2016-10-26 10:15:04 +0200

    First Commit

---
+++ GarageXX/Models/Member.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace GarageXX.Models {
+    public class Member {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public string PNr { get; set; }
+
+        public virtual List<Vehicle> Vehicles { get; set; }
+
+    }
+}
---
+++ GarageXX/Models/Vehicle.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace GarageXX.Models {
+    public class Vehicle {
+        public int Id { get; set; }
+        public string RegNr { get; set; }
+
+        [Required(ErrorMessage = "Regnr måste anges!")]
+        public string Color { get; set; }
+        public DateTime CheckinTime { get; set; }
+        public int Tyres { get; set; }
+        public string Brand { get; set; }
+        public string Model { get; set; }
+
+        public int MemberId { get; set; }
+        public virtual Member Member { get; set; }
+
+        public int VehicleTypeId { get; set; }
+        public virtual VehicleType VehicleType { get; set; }
+
+    }
+}
